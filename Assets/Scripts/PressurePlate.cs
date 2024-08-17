99f5bc4f3060f0a154359b82a34ae67ce55787ec
Nat10k <156871636+NoArt278@users.noreply.github.com>
2024-08-17 23:13:05 +0700

    feat : change lever into pressure plate

---
+++ Assets/Scripts/PressurePlate.cs
@@ -0,0 +1,21 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lever : MonoBehaviour
+{
+    public List<MovablePlatform> movablePlatforms = new List<MovablePlatform>();
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (other.transform.GetComponent<Matryoshka>().isActive)
+            {
+                foreach (var platform in movablePlatforms)
+                {
+                    platform.TriggerMove();
+                }
+            }
+        }
+    }
+}
